b9ab1afc70e2d6bd4e027a7c6cbd3c3ceaef70e1
MarcusTang <tangzhiqiande@gmail.com>
2023-03-07 21:11:22 -0800

    footer finish

---
+++ script.js
@@ -0,0 +1,28 @@
+// Set Footer copyright current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHero);
